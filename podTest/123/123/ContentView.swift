7fe9ac3bd5217a3a5633715b45613149cc34df75
张帅 <zhangshuai@192.168.0.103>
2019-12-22 04:16:24 +0800

    添加初始代码

---
+++ podTest/123/123/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  123
+//
+//  Created by 张帅 on 2019/12/22.
+//  Copyright © 2019 张帅. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
